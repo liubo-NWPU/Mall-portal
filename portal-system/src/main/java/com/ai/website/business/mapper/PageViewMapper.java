21e2aef6950a9eb9a19f4e83c953769672ae5c4c
liubo <liub@geovis.com.cn>
2020-12-23 10:44:24 +0800

    1.0

--- portal-system/src/main/java/com/ai/website/business/mapper/PageViewMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ai.website.business.mapper;
-
-import com.ai.website.business.entity.PageViewEntity;
-
-public interface PageViewMapper {
-
-    int insert(PageViewEntity pageViewEntity);
-}
